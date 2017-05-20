9b486016d6fe9d170d5048e7813774a0df03295f
Vitalij <kontrabasista@wp.pl>
2017-05-20 11:20:32 +0200

    add material design project

--- app/src/main/java/com/radek/materialdesignapp/LoginActivity.java
+++ app/src/main/java/com/radek/materialdesignapp/LoginActivity.java
@@ -14,6 +14,10 @@ import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.EditText;
+import android.widget.ImageView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by Radek on 19.05.2017.
@@ -21,11 +25,19 @@ import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
     private static String mypassword = "ala";
+    @BindView(R.id.android)
+    ImageView imageView;
+    @BindView(R.id.login)
+    EditText login;
+    @BindView(R.id.password)
+    EditText passwordEditText;
+    @BindView(R.id.password_input_layout)
+    TextInputLayout passwordInputLayout;
+    @BindView(R.id.fab)
+    FloatingActionButton fab;
+    @BindView(R.id.coordinatorLayout)
+    CoordinatorLayout coordinatorLayout;
 
-    private EditText passwordEditText;
-    private FloatingActionButton fab;
-    private TextInputLayout passwordInputLayout;
-    private  CoordinatorLayout coordinatorLayout;
     private Animation shake;
 
     private View.OnClickListener myFabListener = new View.OnClickListener() {
@@ -48,11 +60,13 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login_activity_layout);
+        ButterKnife.bind(this);
         initView();
         setView();
         initToolbar();
 
         shake = AnimationUtils.loadAnimation(this, R.anim.shake);
+        imageView.startAnimation(shake);
 
     }
 
@@ -60,12 +74,19 @@ public class LoginActivity extends AppCompatActivity {
         passwordEditText = (EditText) findViewById(R.id.password);
         fab = (FloatingActionButton) findViewById(R.id.fab);
         passwordInputLayout = (TextInputLayout) findViewById(R.id.password_imput_layout);
-        coordinatorLayout = (CoordinatorLayout)findViewById(R.id.coordinatorLayout);
+        coordinatorLayout = (CoordinatorLayout) findViewById(R.id.coordinatorLayout);
+        imageView = (ImageView) findViewById(R.id.android);
 
     }
 
     private void setView() {
         fab.setOnClickListener(myFabListener);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imageView.clearAnimation();
+            }
+        });
 
     }
 
